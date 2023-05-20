30f614d1bdff40f88afab9aae3f953c4c1716cc1
hakim-arbi <hakimarbi525@gmail.com>
2023-05-21 00:07:48 +0100

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
-    <div>Hello</div>
+    <Container>
+    <Sidebar />
+    <MainContent />
+  </Container>
   );
 }
+
+const Container = styled.div`
+display: flex;
+height: 97vh;
+background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
+border-radius: 2rem;
+`;
 
 export default App;
