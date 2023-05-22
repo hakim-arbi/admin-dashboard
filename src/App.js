e21cee77e5d95e6168dbad7dfd2fc30a699cf97d
hakim-arbi <hakimarbi525@gmail.com>
2023-05-22 18:30:40 +0100

    Add MainContent

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ display: flex;
 height: 97vh;
 background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
 border-radius: 2rem;
+@media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+}
 `;
 
 export default App;
--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import {themeColor} from "../utils"
 
-function Badge({content}) {
+function Badge({ content, clean = false, glow = false, paid = false, late = false }) {
   return (
-    <Div>{content}</Div>
+    <Div clean={clean} glow={glow} paid={paid} late={late}>
+      {content}
+    </Div>
   )
 }
 
@@ -15,6 +17,36 @@ const Div = styled.div`
     color: white;
     background-color: ${themeColor};
     cursor: pointer;
+    ${({ clean }) =>
+    clean &&
+    `background-color: transparent;
+    border: 0.05rem solid ${themeColor};
+     color:${themeColor};
+     `}
+    ${({ glow })=> 
+glow && 
+`
+  font-size: 0.8rem;
+  padding: 0.2rem 0.5rem;
+  font-weight: normal;
+  color: #2f233d;
+  background-color: rgba(109, 134, 245, 0.192);
+`}
+ ${({ paid }) =>
+    paid &&
+    `
+        background-color:#70e00041;
+        color:#70e000;
+    `}
+
+${({ late }) =>
+    late &&
+    `
+        background-color:#ff595e41;
+        color:#ff595e;
+    `}
 `;
+
+
 
 export default Badge
